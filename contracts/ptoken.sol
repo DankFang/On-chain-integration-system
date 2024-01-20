893036828311ad959a1f40fc706e5855b5363ec4
DankFang <1975504764@qq.com>
2024-01-20 17:26:37 +0800

    add burn

--- contracts/ptoken.sol
+++ contracts/ptoken.sol
@@ -10,33 +10,36 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "./statusNFT.sol";
-
 contract ptoken is Ownable, ERC20Burnable, ReentrancyGuard {
     uint256 constant tokenAmountOneDay = 10000;
     uint256 public startTime;
     uint256 public claimCooldown = 1 days; // 设置领取冷却时间为1天
     uint256 public ptokenPrice;
-
+    address public statusNFTAddr;
     mapping(address => uint256) public lastClaimTime;
     mapping (address=>bool) isMerchant;
+    mapping(address=>uint) burnedPtoken;
     
     modifier onlyMerchant() {
         bool _isMerchant = isMerchant[msg.sender];
         require(_isMerchant, "You are not merchant");
         _;
     }
-
+    event burnPtoen(address indexed operator,uint indexed amount);
     constructor(
         string memory name, 
-        string memory symbol
+        string memory symbol,
+        address _statusNFTAddr
     ) Ownable(msg.sender) ERC20(name, symbol) {
         startTime = block.timestamp;
+        statusNFTAddr = _statusNFTAddr;
     }
+    
     /**
      * 先要判断领币的是不是nft所有者
      * 领币
      */
-    function claimToken(address statusNFTAddr,uint256 tokenId) external nonReentrant{
+    function claimToken(uint256 tokenId) external nonReentrant{
         uint256 person = statusNFT(statusNFTAddr).getCurrentId();
         address owner = statusNFT(statusNFTAddr).ownerOf(tokenId);
         require(msg.sender == owner,"You are not in this community");
@@ -45,19 +48,10 @@ contract ptoken is Ownable, ERC20Burnable, ReentrancyGuard {
         _mint(msg.sender,amountForOne);
         lastClaimTime[msg.sender] = block.timestamp;
     }
-    // 商家付钱购买ptoken来销毁，3%进入指定地址（手续费）其余的全部注入uniswap池子
-    // 此处得采用chainlink的喂价机制
-    // 
-    function burnToken() external payable onlyMerchant {
 
+    function burnToken(uint burnAmount) external onlyMerchant {
+        ERC20Burnable.burn(burnAmount);
+        burnedPtoken[msg.sender]+=burnAmount;
+        emit burnPtoen(_msgSender(),burnAmount);
     }
-
-    function getPtokenPrice() public view returns (uint256) {
-        return ptokenPrice;
-    }
-
-    function setPtokenPrice() public {
-
-    }
-
 }
